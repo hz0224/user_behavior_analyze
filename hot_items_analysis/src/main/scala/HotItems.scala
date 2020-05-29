89f669d0315249f8ec131c252d04c42564f9df77
Administrator <hangzhou.l@winbaoxian.com>
2020-05-29 13:37:31 +0800

    热门商品topN

--- hot_items_analysis/src/main/scala/HotItems.scala
+++ hot_items_analysis/src/main/scala/HotItems.scala
@@ -1,10 +1,16 @@
+import java.sql.Timestamp
+
 import org.apache.flink.api.common.functions.AggregateFunction
+import org.apache.flink.api.common.state.{ListState, ListStateDescriptor}
 import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.scala.function.WindowFunction
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.util.Collector
+
+import scala.collection.mutable.ArrayBuffer
 
 /**
   * Created by Liu HangZhou on 2020/05/27
@@ -43,9 +49,9 @@ object HotItems {
     //CountAgg是增量聚合函数 ， ItemCountWindowResult是窗口函数,可以拿到增量聚合的结果和该窗口的信息，它的输入数据是增量聚合函数CountAgg的输出。
     //因此此时的窗口函数计算的就不是整个窗口内的所有数据了，它只是对预聚合的结果进行一个包装，加上了该窗口的信息。相当于将增量聚合和窗口函数进行了整合。
 
-
-
-    aggDStream.print()
+    //对窗口进行分组并排序取topN
+    val resultDStream = aggDStream.keyBy(_.windowEnd).process(new TopNHotItems(5))
+    resultDStream.print()
 
     env.execute("hot_items_job")
   }
@@ -76,5 +82,53 @@ class ItemCountWindowResult extends WindowFunction[Long, ItemViewCount, Long, Ti
     val count = input.iterator.next()
     //使用 out.collect(输出结果类型)输出数据
     out.collect(ItemViewCount(itemId,count,end))
+  }
+}
+
+
+//自定义 KeyedProcessFunction<K, I, O>     K: key  I: in O: out
+class TopNHotItems(n: Int) extends KeyedProcessFunction[Long,ItemViewCount,String]{
+  //定义一个listState，用来保存当前窗口所有的 count结果.
+  lazy val itemCountListState: ListState[ItemViewCount] = getRuntimeContext.getListState(new ListStateDescriptor[ItemViewCount]("itemcount-list",classOf[ItemViewCount]))
+
+  override def processElement(value: ItemViewCount, context: KeyedProcessFunction[Long, ItemViewCount, String]#Context, collector: Collector[String]): Unit = {
+    //每来一条数据，就把它保存在状态中.
+    itemCountListState.add(value)
+    //注册定时器，在 windowEnd + 100 触发.
+    context.timerService().registerEventTimeTimer(value.windowEnd + 100)
+  }
+
+  //定时器触发时，从状态中取数据然后排序输出.
+  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[Long, ItemViewCount, String]#OnTimerContext, out: Collector[String]): Unit = {
+    val allItemCountBuffer = new ArrayBuffer[ItemViewCount]()
+
+    import scala.collection.JavaConversions._
+    //将状态中的数据加到 allItemCountBuffer中.
+    for(itemCount <- itemCountListState.get()){
+      allItemCountBuffer += itemCount
+    }
+
+    //按照count值从大到小排序取TopN
+    val sortedItemCount = allItemCountBuffer.sortWith(_.count > _.count).take(n)
+
+    //清空状态.
+    itemCountListState.clear()
+
+    //将排名信息格式成String，方便查看
+    val sb = new StringBuilder
+    sb.append("时间:").append(new Timestamp(timestamp - 100)).append("\n")
+
+    for(i <- 0 until sortedItemCount.length){
+      sb.append("Top").append(i+1).append(":")
+        .append("itemId").append(sortedItemCount(i).itemId)
+        .append("hot").append(sortedItemCount(i).count)
+        .append("\n")
+    }
+    sb.append("=================================================\n\n")
+
+    //控制打印频率，方便查看
+    Thread.sleep(1000)
+
+    out.collect(sb.toString())
   }
 }
